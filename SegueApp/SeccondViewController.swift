fb4f1a74b3f0e508a5648c8a7a596c470520d48b
Can Seyhan <muhamedseyhan503@gmail.com>
2020-11-09 01:26:51 +0300

    Full Project

--- SegueApp/SeccondViewController.swift
+++ SegueApp/SeccondViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class SeccondViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mySeccondViewLabel: UILabel!
+    
+    var myName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nameLabel.text = myName
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userName = ""
+    
+    @IBOutlet weak var nameLabel: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad function called")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear function called")
+        nameLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear function called")
     }
 
-
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear function called")
+        
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+        userName = nameLabel.text!
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            // as
+            let destinationVC = segue.destination as! SeccondViewController
+            destinationVC.myName = userName
+        }
+    }
+    
 }
 
